ab72770d73b4dc0868b11be710f9cea942f24d08
Andrew <svt.andrey1631@gmail.com>
2020-03-01 16:57:52 +0200

    qqwertyuio:

--- Teamwork/Teamwork.cpp
+++ Teamwork/Teamwork.cpp
@@ -6,18 +6,8 @@ int main()
 	cout << "Second" << endl;
 	for (int i = 0; i < 5; i++)
 		cout << i + 1 << " ";
+	cout << 1213;
 }
-void sort_min(int* temp, int t)
-{
-	int min;
-	for (int i = 0; i < t - 1; i++)
-	{
-		min = temp[i];
-		for (int j = i + 1; j < t; j++)
-		{
-			if (temp[j] < min) { min = temp[j]; temp[j] = temp[i]; temp[i] = min; }
-		}
-	}
-}
+
 
 
