a8e164add9b3f0df26ea1b0b7d1271fdd1a3534c
Andrew <svt.andrey1631@gmail.com>
2020-03-01 15:31:54 +0200

    Добавлена функция сортировки массива

--- Teamwork/Teamwork.cpp
+++ Teamwork/Teamwork.cpp
@@ -6,4 +6,17 @@ int main()
    cout << "Second" << endl;
 }
 
+void sort_min(int* temp, int t)
+{
+	int min;
+	for (int i = 0; i < t - 1; i++)
+	{
+		min = temp[i];
+		for (int j = i + 1; j < t; j++)
+		{
+			if (temp[j] < min) { min = temp[j]; temp[j] = temp[i]; temp[i] = min; }
+		}
+	}
+}
+
 
